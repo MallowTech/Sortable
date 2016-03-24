2b055a283742f52fc03bff25ba4c8ae79adc539b
Vadivel Jeganathan <vadivel@mallow-tech.com>
2016-03-24 17:46:31 +0530

    UI refinements in the card - Added sample icons, modified layout based on clients requirements.

--- st/app-mallowtech.js
+++ st/app-mallowtech.js
@@ -1,29 +1,30 @@
 //Customised by MallowTech
 (function () {
 	'use strict';
-    Sortable.create(document.getElementById('cards'), {
+    Sortable.create(document.getElementById('cards-title'), {
 			sort: false,
 			group: {
-                    name: 'card-system',
+                    name: 'cards-box',
                     pull: 'clone',
                     put: false
                 },
 			animation: 150
 		});
-    Sortable.create(document.getElementById('cards-box'), {
+    Sortable.create(document.getElementById('cards'), {
 			sort: true,
 			group: {
-                name: 'card-system',
+                name: 'cards-box',
                 pull: false,
                 put: true
             },
             handle: '.drag-handle',
 			animation: 150,
             onAdd: function (evt) {
-                if($(evt.item).closest('#cards-box').length){
+                if($(evt.item).closest('#cards').length){
                     var target = $(evt.item).data('card');
                     (evt.item).innerHTML = $(target).html();
                     $(evt.item).removeClass('hide-card');
+                    $(evt.item).removeClass('card-chip');
                     $(evt.item).addClass('show-card');
                 }
             }
